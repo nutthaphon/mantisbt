546c21ae4dc21e5283584bafc2d5d469e7de9dc1
nutthaphon <nutthaphon@gmail.com>
2018-09-26 10:23:05 +0700

    ok

--- html/plugins/SourceSVN/SourceSVN.php
+++ html/plugins/SourceSVN/SourceSVN.php
@@ -80,7 +80,13 @@ class SourceSVNPlugin extends MantisSourcePlugin {
 	public function url_file( $p_repo, $p_changeset, $p_file ) {}
 
 	public function url_diff( $p_repo, $p_changeset, $p_file ) {
-		return "http://www.v-smart.comm";
+		//$t_root = $p_repo->info['hub_root'];
+		//$t_reponame = $p_repo->info['hub_reponame'];
+		//$t_ref = $p_changeset->revision;
+		//$t_filename = $p_file->filename;
+		//$results = print_r($p_repo, true); 
+		//echo '<script>console.log("'.$results.'")</script>';
+		//return "http://www.v-smart.com";
 	}
 
 	public function update_repo_form( $p_repo ) {
